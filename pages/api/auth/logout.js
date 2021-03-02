090a5b984f3709a193a80039b93fc3c645238770
Rashmi V A <rashmi.va@renderpub.com>
2021-03-02 16:27:36 +0530

    added console logs

--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -1,5 +1,5 @@
 async function handleLogout(req, res) {
-
+  console.log("in api/auth/logout");
   
   // res.redirect(`https://${process.env.COGNITO_DOMAIN}/logout?response_type=code&client_id=${process.env.COGNITO_CLIENT_ID}&logout_uri=${process.env.COGNITO_LOGOUT_URL}&redirect_uri=${process.env.COGNITO_LOGOUT_REDIRECT_URL}`);
 

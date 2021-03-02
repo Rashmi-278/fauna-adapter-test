73a4e943eadcfe3222d8caf9cf3b5163189cc7fe
Rashmi V A <rashmi.va@renderpub.com>
2021-03-02 17:35:21 +0530

    removed server log comments

---
+++ pages/api/auth/fed-logout.js
@@ -0,0 +1,25 @@
+import jwt from "next-auth/jwt"
+// import log from "utils/server-logger"
+
+export default async function federatedLogout(req, res) {
+  try {
+    const token = await jwt.getToken({req, secret: process.env.SECRET, encryption: true })
+    if (!token) {
+    console.log("No JWT token found when calling /federated-logout endpoint")
+      return res.redirect(process.env.NEXTAUTH_URL)
+    }
+    if (!token.idToken)
+     console.log("Without an id_token the user won't be redirected back from the IdP after logout.")
+
+    const endsessionURL = `https://${process.env.COGNITO_DOMAIN}/connect/endsession`
+    const endsessionParams = new URLSearchParams({
+      id_token_hint: token.idToken,
+      post_logout_redirect_uri: process.env.NEXTAUTH_URL,
+    })
+    return res.redirect(`${endsessionURL}?${endsessionParams}`)
+  } catch (error) {
+    // log.error(error)
+    console.log(error)
+    res.redirect(process.env.NEXTAUTH_URL)
+  }
+}

35e03e96421b10b5cb73c147815688a289719033
Rashmi V A <rashmi.va@renderpub.com>
2021-03-04 21:46:49 +0530

    href cognito logout endpoint

--- components/header.js
+++ components/header.js
@@ -1,12 +1,28 @@
 import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './header.module.css'
-//import logout from '../pages/api'
 import federatedLogout from '../pages/api/auth/fed-logout'
 
 // The approach used in this component shows how to built a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
+
+
+
+ async function federatedLogout(req, res) {
+  try {
+    
+    
+
+    const endsessionURL = `https://${process.env.COGNITO_DOMAIN}/logout?&client_id=${process.env.COGNITO_CLIENT_ID}&logout_uri=${process.env.COGNITO_LOGOUT_URL}`
+   
+    return res.redirect(`${endsessionURL}`)
+  } catch (error) {
+
+    console.log(error)
+    res.redirect(process.env.NEXTAUTH_URL)
+  }
+}   
 export default function Header () {
   const [ session, loading ] = useSession()
   
@@ -37,13 +53,13 @@ export default function Header () {
               <strong>{session.user.email || session.user.name}</strong>
               </span>
             <a
-                //href={`/api/auth/signout`}
+                href={`https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/`}
                 className={styles.button}
                 onClick={(e) => {
                   e.preventDefault()
                   
                   signOut()
-                  federatedLogout()
+                  //federatedLogout()
                   
                   console.log("signed out")
                 }}
--- pages/api/auth/fed-logout.js
+++ pages/api/auth/fed-logout.js
@@ -3,22 +3,13 @@ import jwt from "next-auth/jwt"
 
 export default async function federatedLogout(req, res) {
   try {
-    // const token = await jwt.getToken({req, secret: process.env.SECRET, encryption: true })
-    // if (!token) {
-    // console.log("No JWT token found when calling /federated-logout endpoint")
-    //   return res.redirect(process.env.NEXTAUTH_URL)
-    // }
-    // if (!token.idToken)
-    //  console.log("Without an id_token the user won't be redirected back from the IdP after logout.")
+   
 
     const endsessionURL = `https://${process.env.COGNITO_DOMAIN}/logout?&client_id=${process.env.COGNITO_CLIENT_ID}&logout_uri=${process.env.COGNITO_LOGOUT_URL}`
-    // const endsessionParams = new URLSearchParams({
-    //   id_token_hint: token.idToken,
-    //   post_logout_redirect_uri: process.env.NEXTAUTH_URL,
-    // })
+    
     return res.redirect(`${endsessionURL}`)
   } catch (error) {
-    // log.error(error)
+    
     console.log(error)
     res.redirect(process.env.NEXTAUTH_URL)
   }

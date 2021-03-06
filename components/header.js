83bf4a1fb8f6c14fbfb81c55d7c3b8e4eb652223
Rashmi V A <rashmi.va@renderpub.com>
2021-03-06 20:59:25 +0530

    header.js refreshed

--- components/header.js
+++ components/header.js
@@ -1,19 +1,13 @@
 import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/client'
 import styles from './header.module.css'
-import { Router, useRouter } from 'next/router'
-import redirect from 'nextjs-redirect'
-import {useState} from 'react';
-import axios from 'axios';
 
-
-const cognito = redirect('https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/')
-
+// The approach used in this component shows how to built a sign in and sign out
+// component that works on pages which support both client and server side
+// rendering, and avoids any flash incorrect content on initial page load.
 export default function Header () {
   const [ session, loading ] = useSession()
-  const router = useRouter();
-  const [out , setSignout] = useState(false)
-
+  
   return (
     <header>
       <noscript>
@@ -28,7 +22,7 @@ export default function Header () {
                 className={styles.buttonPrimary}
                 onClick={(e) => {
                   e.preventDefault()
-                  signIn("cognito")
+                  signIn('cognito')
                 }}
               >
                 Sign in
@@ -40,66 +34,16 @@ export default function Header () {
               <small>Signed in as</small><br/>
               <strong>{session.user.email || session.user.name}</strong>
               </span>
-            {/* <a
-                href={'/api/auth/signout'}
+            <a
+                href={`https://nextauth1.auth.ap-south-1.amazoncognito.com/error?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/api/auth/logout&redirect_uri=https://fauna-adapter-test.vercel.app/api/auth/signout`}
                 className={styles.button}
-                onClick={ async(e) => {
+                onClick={(e) => {
                   e.preventDefault()
                   signOut()
-                  // const nextauthdata = await signOut({redirect:false} )
-                  // console.log("1")
-                  // console.log(nextauthdata)
-
-
-                  // const res = await axios.get(`https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/` , { headers: {"Access-Control-Allow-Origin": "*"} });
-                  // console.log(res)
-                  
-                  // const { signoutdata, error } = useSWR('/api/auth/logout', fetcher)
-                  // console.log(signoutdata)
-
-
-                  
-
-                  // fetch('https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/' , { 'method' : 'GET' , 'mode':'no-cors' , 'credentials': 'include'})
-                  // .then(response => { console.log("2"); console.log(response) }) ;
-
-                  
-                  
-                 // router.push('/redirect')                  
-
-                  //const signoutdata  = await signOut({ callbackUrl: "/api/auth/logout"})        
-                            
-                  //router.push('https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/')                  
-                } }
-              >
-                Sign out
-              </a> */}
-
-
-              <a
-                href={`https://nextauth1.auth.ap-south-1.amazoncognito.com/logout?client_id=7agnle801a00muhiuvc26n6rfu&logout_uri=https://fauna-adapter-test.vercel.app/api/auth/signout`}
-                className={styles.button}
+                }}
               >
                 Sign out
               </a>
-{/* 
-              <a
-              href={'/api/auth/signout'}
-              className={styles.button}
-              onClick={(e) => {
-                e.preventDefault()
-                  signOut()
-              }}             
-              >
-                signout
-              </a> */}
-
-              {/* <button  
-              className={styles.button}
-              onClick={() => window.location.href = "/api/auth/logout"}>
-              Sign out
-              </button> */}
-              
           </>}
         </p>
       </div>
